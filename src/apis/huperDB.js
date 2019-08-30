fb013c68a8d823dd9c39548a8bcddeca36323669
hupiy14 <46478008+hupiy14@users.noreply.github.com>
2019-08-30 18:50:21 +0200

    Update huperDB.js

--- src/apis/huperDB.js
+++ src/apis/huperDB.js
@@ -4,44 +4,14 @@
 ///firebase testing huputy
 
 export const config = {
-    apiKey: 'AIzaSyDKEIQRnNYczx3IZgiLwIucSBCBLg1t0Wg',
-    authDomain: 'upity-9b190.firebaseapp.com',
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
     databaseURL: 'https://hupity-9b190.firebaseio.com',
     projectId: 'hupity-9b190',
     storageBucket: 'hupity-9b190.appspot.com',
-    messagingSenderId: '874067485777'
+    messagingSenderId: 'XXXXXXXXXXXXXXXXXX'
 }
 
-///firebase desarrollo 
-
-/*
-export const config = {
-    apiKey: "AIzaSyAKC1R5XUU8UhfQB8bJB0xc1IPgf2nwFS8",
-    authDomain: "pruebahuper.firebaseapp.com",
-    databaseURL: "https://pruebahuper.firebaseio.com",
-    projectId: "pruebahuper",
-    storageBucket: "pruebahuper.appspot.com",
-    messagingSenderId: "328282757234"
-  };
-
-
-*/
-
-/*
-  export const config = {
-    apiKey: "AIzaSyB-2Y-livEMq0CUmCBtcCCtk-gkcwoONOc",
-    authDomain: "hupity-visual.firebaseapp.com",
-    databaseURL: "https://hupity-visual.firebaseio.com",
-    projectId: "hupity-visual",
-    storageBucket: "hupity-visual.appspot.com",
-    messagingSenderId: "80713476876",
-    appId: "1:80713476876:web:0116352d070bc423"
-  };
-*/
-//firebase.initializeApp(config);
-//firebase.firestore().settings({ timestampsInSnapshots: true });
-
-//export default firebase;
 
 
 
